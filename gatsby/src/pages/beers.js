79b874a8c4f50db7eb9dfb2e2fa9526163ee274e
Joe Ziemba <j.ziemba@elsevier.com>
2020-10-22 09:01:22 -0400

    pages and layouts

---
+++ gatsby/src/pages/beers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function BeersPage() {
+  return (
+    <Layout>
+      <h1>Beers</h1>
+    </Layout>
+  );
+}
---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Nav from '../components/Nav';
+
+function HomePage() {
+  return (
+    <Layout>
+      <p>Hey it's the home page</p>
+    </Layout>
+  );
+}
+
+export default HomePage;
